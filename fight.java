e750d03af272138c492fe514d92404d509524b3a
Reggie <reganlussier@gmail.com>
2018-03-05 14:56:41 -0600

    Create fight.java

---
+++ fight.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static String fight;
+    
+	public static void main(String[] args) {
+	    
+	    aString = "Ready Fight!";
+		system.out.println(fight);
+		aString = "Try again next time.";
+		system.out.println(fight);
+		
+		
+	}
+}
